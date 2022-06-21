00ce2506fbd71e01c93e1b5082543d6832abf23b
Naptoss <tomgabinio@gmail.com>
2022-06-21 16:38:08 -0300

    puppet

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-]
+    path('<int:contato_id>',views.ver_contato,name='ver_contato'), # aqui e um novo endpoint 
+]          # aqui ele ta buscando o id do contato
--- contatos/views.py
+++ contatos/views.py
@@ -3,7 +3,14 @@ from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.all() # aqui esta selecionando todos os itens da base de dados
     return render(request,'contatos/index.html',{
         'contatos': contatos
     })
+
+
+def ver_contato(request, contato_id):
+    contato = Contato.objects.get(id=contato_id) 
+    return render(request,'contatos/ver_contato.html',{
+        'contato': contato
+    }) 
