ca67bbc8e6edbcae7afc672dca78669f31a29f8b
Naptoss <tomgabinio@gmail.com>
2022-06-22 20:21:58 -0300

    .

--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Contato
-
+from django.http import Http404
 
 def index(request):
     contatos = Contato.objects.all() # aqui esta selecionando todos os itens da base de dados
@@ -10,7 +10,8 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id) 
+    contato = get_list_or_404(Contato,id=contato_id) # aqui ele verifica se o id ta registrado, se nao tiver ele retorna o erro 404 
     return render(request,'contatos/ver_contato.html',{
-        'contato': contato
+            'contato': contato
     }) 
+   
